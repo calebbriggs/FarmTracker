66855d285c4011405be67ffb91c4a93502cbf1e8
Caleb Briggs <caleb@ninecollective.com>
2011-09-20 15:25:34 -0500

    farmtracker is on production

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,8 @@ module ApplicationHelper
     # selection for a year value
   # ‘f’ represents the passed in form value
   Recaptcha.configure do |config|
-    config.public_key  = '6LcdVcgSAAAAAIyZUQdqFkkudMthzI2tWJwZYNeO'
-    config.private_key = '6LcdVcgSAAAAALIM7gPNdC_pY-DOQJ_iSj_Xrlre'
+    config.public_key  = '6Le6VsgSAAAAAP4oLNSt9hqjFZmcl_d0oMelRI-5'
+    config.private_key = '6Le6VsgSAAAAAFaxE-FD-O0vEDI2IFMd0PhTvBf2'
 
   end
 end
